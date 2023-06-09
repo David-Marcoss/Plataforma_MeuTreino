6c866294aee3c605c92f10b50e37310abd2f558c
David Marcos <ddragondavi@gmail.com>
2023-06-09 17:02:03 -0300

    - Configurando o ambiente de desenvolvimento e implementação tela home

---
+++ apps/core/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.views.generic import CreateView
+from django.contrib.auth.models import User,Group
+from django.urls import reverse_lazy
+#from .forms import UserCreationForm
+
+
+def homeView(request):      
+    return render(request,template_name='home.html')
